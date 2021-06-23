9aec87b821003dd2e4d4f07e8c6b1c9debd012d1
undersfx <atchin_@hotmail.com>
2021-06-23 19:10:43 -0300

    fix: update_dotfiles bug

--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -11,10 +11,9 @@ NOW=$(date +%d_%m_%Y_%T)
 for DOTFILE in $(ls -a ~/dotfiles/src/ | grep -iE '^\.[a-z]'); do 
 	if [ -e ~/$DOTFILE ]
 	then
-		#mv ~/$DOTFILE ~/""$DOTFILE"_"$NOW""
-		echo $'\n' >> ~/$DOTFILE
-		echo "# - - - Config updated at: $NOW - - -" >> ~/$DOTFILE
-		echo ~/dotfiles/src/$DOTFILE >> ~/$DOTFILE
+		cp ~/$DOTFILE ~/""$DOTFILE".bkp."$NOW""
+		echo '' >> ~/$DOTFILE
+		cat ~/dotfiles/src/$DOTFILE >> ~/$DOTFILE
 		echo "$DOTFILE updated."
 	else
 		cp ~/dotfiles/src/$DOTFILE ~
